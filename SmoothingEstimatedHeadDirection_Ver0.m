db8211b5e763670bebf5c8f7ca6e427ae774c8a8
Or Pinchasov <or.pinchasov@gmail.com>
2017-01-03 15:11:20 +0200

    Added some error handling to enable running the file without the saved variables

--- SmoothingEstimatedHeadDirection_Ver0.m
+++ SmoothingEstimatedHeadDirection_Ver0.m
@@ -3,8 +3,12 @@ hsize = 5;
 gaussian_kernel=exp(-(([1:hsize]-(hsize+1)/2).^2)/(2*sigma^2));
 gaussian_kernel=gaussian_kernel./sum(gaussian_kernel);
 
-load('angle_per_temporal_bin')
-load('estimated_angle_by_clustering')
+try
+    load('angle_per_temporal_bin')
+    load('estimated_angle_by_clustering')
+catch ME
+    ME
+end
 
 smoothed_estimated_angle_by_clustering=angle(conv2(exp(1i*estimated_angle_by_clustering),gaussian_kernel,'same'));
 smoothed_estimated_angle_by_clustering=mod(smoothed_estimated_angle_by_clustering,2*pi);
