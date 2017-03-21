bb0d5d7b46d1c42c18bef34c4add29f668abf384
Or Pinchasov <or.pinchasov@gmail.com>
2017-03-21 17:57:18 +0200

    Added clustering methods.

--- barcodes_for_clustering.m
+++ barcodes_for_clustering.m
@@ -2,6 +2,10 @@
 
 load('HippDataForOr_7p3p17.mat');
 
+%% Load ACC data
+
+load('ACC_DataForOr_7p3p17');
+
 %% Filter according to distances between points
 
 d = squareform(pdist(v2(:, 2:4)));
@@ -12,8 +16,8 @@ d2 = d;
 d2(d2 == 0) = 1;
 
 % Filter points of v2 according to density factor
-v2_dense = v2(min(d2) < 0.0002, :);
-
+v2_dense = v2(min(d2) < 0.1, :);
+v2_is_dense_vec=(min(d2) < 0.1);
 % Plot
 figure; scatter3(v2_dense(:, 2), v2_dense(:, 3), v2_dense(:, 4), 5, '.');
 
@@ -112,7 +116,7 @@ for i = 2:length(r_vec)
     indices = new_ind_array{i};
     occurrences = occurrences_array{i};
     
-    indices = indices(occurrences > 100);
+    indices = indices(occurrences > 200);
     
     plot(r_vec(i) * ones(length(indices), 1), indices, 'k*');
 end
@@ -162,4 +166,104 @@ while 1
     figure(101);
     chosen_points=~~(all_bins_mat(:,r_index)==all_bins_mat(cluster_index,r_index));
     plot3(data(chosen_points, 1), data(chosen_points, 2), data(chosen_points, 3), '.','color',color_map(n,:),'MarkerSize',5);
+end
+
+%% Plot clusters by r
+
+NUMBER_OF_CLUSTERS = 5;
+
+r = 0.00109;
+
+figure;
+hold on;
+
+scatter3(data(:, 1), data(:, 2), data(:, 3), 5, '.');
+
+[~,r_index]=min(abs(r_vec - r));
+
+indices = new_ind_array{r_index};
+occurrences = occurrences_array{r_index};
+    
+indices = indices(occurrences > 200);
+
+color_map=lines(length(indices));
+
+labels = ones(size(v2_dense, 1), 1) * (NUMBER_OF_CLUSTERS + 1);
+
+for i = 1:length(indices)  
+    chosen_points=~~(all_bins_mat(:,r_index)==all_bins_mat(indices(i),r_index));
+    scatter3(data(chosen_points, 1), data(chosen_points, 2), data(chosen_points, 3), 20, color_map(i,:), '.');
+    
+    labels(chosen_points) = i;
+end
+
+%% Scatter data with clustered data
+color_map_with_unclustered = lines(length(indices) + 1);
+scatter3(data(:, 2), data(:, 3), data(:, 4), 20, color_map_with_unclustered(labels, :), '.');
+
+%%
+load_movie_flag=1;
+if load_movie_flag
+    obj = VideoReader(['Z:\Short term data storage\Lab members\Alon\Yoav C69M1\Day3_all_trials.avi']);
+    obj.Name 
+    im = obj.read;
+end
+%%
+im_active=im(:,:,:,legitimacy_vec);
+%im_active_BW=im2bw(im_active,);
+
+im_active_BW=nan([size(im_active,1) size(im_active,2) size(im_active,4)]);
+for run_frames=1:size(im_active,4)
+    im_active_gray(:,:,run_frames)=rgb2gray(im_active(:,:,:,run_frames));
+end
+
+%% frame_snapshot_for_actvity_data_point (from this point not crucial for creation of figure)
+
+dim_1=2;
+dim_2=3;
+% 
+% figure;
+% axes('position',[0 0.15 1 0.85]);
+% scatter(v2(:,dim_1),v2(:,dim_2),5,cmap_clusters(ind,:),'fill')
+% %scatter(v2(:,dim_1),v2(:,dim_2),1,'k','fill')
+
+while 1
+[x_frame y_frame]=ginput(1);
+dist_vec=sum((v2(:,[dim_1 dim_2])-repmat([x_frame y_frame],[size(v2,1) 1])).^2,2);
+[~,chosen_frame_ind]=min(dist_vec);
+hold on
+plot(v2(chosen_frame_ind,dim_1),v2(chosen_frame_ind,dim_2),'k*')
+
+
+axes('position',[0 0 1 0.15]);
+imagesc(im_active_gray(:,:,chosen_frame_ind))
+colormap gray
+chosen_frame_ind
+end
+%%
+ind = labels;
+
+data = v2_dense;
+
+dim_1=2;
+dim_2=3;
+
+cmap_clusters = lines(NUMBER_OF_CLUSTERS + 1);
+
+figure;
+axes('position',[0 0.15 1 0.85]);
+scatter(v2(:,dim_1),v2(:,dim_2),5,cmap_clusters(ind,:),'fill')
+%scatter(v2(:,dim_1),v2(:,dim_2),1,'k','fill')
+
+while 1
+[x_frame y_frame]=ginput(1);
+dist_vec=sum((v2(:,[dim_1 dim_2])-repmat([x_frame y_frame],[size(v2,1) 1])).^2,2);
+[~,chosen_frame_ind]=min(dist_vec);
+hold on
+plot(v2(chosen_frame_ind,dim_1),v2(chosen_frame_ind,dim_2),'k*')
+
+
+axes('position',[0 0 1 0.15]);
+imagesc(im_active_gray(:,:,chosen_frame_ind))
+colormap gray
 end
